19aa34b0e68deacb23b5210a1e16597127b02676
Gergely Svigruha <sgergo88@gmail.com>
2022-02-12 22:50:11 -0500

    Refactor code

--- model/economy/personal_tasks.go
+++ model/economy/personal_tasks.go
@@ -1,7 +1,6 @@
 package economy
 
 import (
-	"medvil/model/artifacts"
 	"medvil/model/navigation"
 	"medvil/model/time"
 )
@@ -78,62 +77,4 @@ func (t *GoHomeTask) Blocked() bool {
 
 func (t *GoHomeTask) Name() string {
 	return "gohome"
-}
-
-var fruit = artifacts.GetArtifact("fruit")
-var vegetable = artifacts.GetArtifact("vegetable")
-var bread = artifacts.GetArtifact("bread")
-var meat = artifacts.GetArtifact("meat")
-
-var water = artifacts.GetArtifact("water")
-var wine = artifacts.GetArtifact("wine")
-var beer = artifacts.GetArtifact("beer")
-
-func HasFood(r artifacts.Resources) bool {
-	return AvailableFood(r) != nil
-}
-
-func HasDrink(r artifacts.Resources) bool {
-	return AvailableDrink(r) != nil
-}
-
-func AvailableFood(r artifacts.Resources) []*artifacts.Artifact {
-	var available []*artifacts.Artifact = nil
-	for _, a := range []*artifacts.Artifact{fruit, vegetable, bread, meat} {
-		if q, ok := r.Artifacts[a]; ok {
-			if q > 0 {
-				available = append(available, a)
-			}
-		}
-	}
-	return available
-}
-
-func AvailableDrink(r artifacts.Resources) []*artifacts.Artifact {
-	var available []*artifacts.Artifact = nil
-	for _, a := range []*artifacts.Artifact{water, wine, beer} {
-		if q, ok := r.Artifacts[a]; ok {
-			if q > 0 {
-				available = append(available, a)
-			}
-		}
-	}
-	return available
-}
-
-type PersonStateChange struct {
-	Food      uint8
-	Water     uint8
-	Happiness uint8
-	Health    uint8
-}
-
-var ArtifactToPersonState = map[*artifacts.Artifact]PersonStateChange{
-	fruit:     PersonStateChange{Food: 100, Water: 50, Happiness: 50, Health: 50},
-	vegetable: PersonStateChange{Food: 150, Water: 0, Happiness: 0, Health: 50},
-	bread:     PersonStateChange{Food: 200, Water: 0, Happiness: 0, Health: 0},
-	meat:      PersonStateChange{Food: 200, Water: 0, Happiness: 50, Health: 0},
-	water:     PersonStateChange{Food: 0, Water: 200, Happiness: 0, Health: 0},
-	wine:      PersonStateChange{Food: 0, Water: 150, Happiness: 100, Health: 50},
-	beer:      PersonStateChange{Food: 0, Water: 150, Happiness: 100, Health: 0},
 }
