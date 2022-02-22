d58a1f176f0362ebf05a751a1a9370d9a58b5891
Gergely Svigruha <gsvigruha@users.noreply.github.com>
2022-02-21 23:08:24 -0500

    Add sawing/board (#40)

--- view/gui/labels.go
+++ view/gui/labels.go
@@ -48,6 +48,29 @@ func (l *ImageLabel) Render(cv *canvas.Canvas) {
 	}
 }
 
+type DoubleImageLabel struct {
+	X       float64
+	Y       float64
+	SX      float64
+	SY      float64
+	Icon    string
+	SubIcon string
+	Style   uint8
+}
+
+func (l *DoubleImageLabel) Render(cv *canvas.Canvas) {
+	if l.Style == ImageLabelStyleHighlight {
+		cv.SetFillStyle(color.RGBA{R: 224, G: 240, B: 255, A: 240})
+		cv.FillRect(l.X, l.Y, l.SX, l.SY)
+	}
+	cv.DrawImage("icon/gui/"+l.Icon+".png", l.X, l.Y, l.SX, l.SY)
+	cv.DrawImage("icon/gui/"+l.SubIcon+".png", l.X, l.Y, l.SX/2, l.SY/2)
+	if l.Style == ImageLabelStyleDisabled {
+		cv.SetFillStyle(color.RGBA{R: 0, G: 0, B: 0, A: 64})
+		cv.FillRect(l.X, l.Y, l.SX, l.SY)
+	}
+}
+
 type ScaleLabel struct {
 	X      float64
 	Y      float64
@@ -61,7 +84,11 @@ type ScaleLabel struct {
 func (l *ScaleLabel) Render(cv *canvas.Canvas) {
 	cv.DrawImage("icon/gui/"+l.Icon+".png", l.X, l.Y, l.SX, l.SY)
 	cv.SetFillStyle("#B00")
-	cv.FillRect(l.X+l.SX, l.Y+l.SY, l.ScaleW, -l.SY*l.Scale)
+	var s = l.Scale
+	if s >= 1.0 {
+		s = 1.0
+	}
+	cv.FillRect(l.X+l.SX, l.Y+l.SY, l.ScaleW, -l.SY*s)
 }
 
 type TextureLabel struct {
