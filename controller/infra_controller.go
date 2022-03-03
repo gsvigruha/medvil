098c98367dca657429efdd7db3d6420ec3ae2cf9
Gergely Svigruha <sgergo88@gmail.com>
2022-03-03 01:06:29 -0500

    Small UI controlelr fixes

--- controller/infra_controller.go
+++ controller/infra_controller.go
@@ -2,6 +2,7 @@ package controller
 
 import (
 	"github.com/tfriedel6/canvas"
+	"image/color"
 	"medvil/model/building"
 	"medvil/renderer"
 	"medvil/view/gui"
@@ -31,6 +32,10 @@ func (b InfraBuildButton) Click() {
 
 func (b InfraBuildButton) Render(cv *canvas.Canvas) {
 	b.b.Render(cv)
+	if b.ic.it != b.it {
+		cv.SetFillStyle(color.RGBA{R: 64, G: 0, B: 0, A: 128})
+		cv.FillRect(b.b.X, b.b.Y, 32, 32)
+	}
 }
 
 func (b InfraBuildButton) Contains(x float64, y float64) bool {
@@ -40,15 +45,12 @@ func (b InfraBuildButton) Contains(x float64, y float64) bool {
 func (ic *InfraController) HandleClick(c *Controller, rf *renderer.RenderedField) bool {
 	if ic.it == InfraTypeNone {
 		rf.F.Road = building.Road{T: nil}
-		return true
-	} else if ic.it == InfraTypeDirtRoad && rf.F.Walkable() {
+	} else if ic.it == InfraTypeDirtRoad && rf.F.Walkable() && rf.F.Buildable() {
 		rf.F.Road = building.Road{T: building.DirtRoadType}
-		return true
-	} else if ic.it == InfraTypeCobbleRoad && rf.F.Walkable() {
+	} else if ic.it == InfraTypeCobbleRoad && rf.F.Walkable() && rf.F.Buildable() {
 		rf.F.Road = building.Road{T: building.CobbleRoadType}
-		return true
 	}
-	return false
+	return true
 }
 
 func InfraToControlPanel(cp *ControlPanel) {
@@ -56,13 +58,19 @@ func InfraToControlPanel(cp *ControlPanel) {
 	ic := &InfraController{it: InfraTypeNone}
 
 	p.AddButton(InfraBuildButton{
-		b:  gui.ButtonGUI{Texture: "infra/dirt_road", X: float64(10), Y: float64(InfraPanelTop), SX: 32, SY: 32},
+		b:  gui.ButtonGUI{Texture: "terrain/grass", X: float64(10), Y: float64(InfraPanelTop), SX: 32, SY: 32},
+		it: InfraTypeNone,
+		ic: ic,
+	})
+
+	p.AddButton(InfraBuildButton{
+		b:  gui.ButtonGUI{Texture: "infra/dirt_road", X: float64(50), Y: float64(InfraPanelTop), SX: 32, SY: 32},
 		it: InfraTypeDirtRoad,
 		ic: ic,
 	})
 
 	p.AddButton(InfraBuildButton{
-		b:  gui.ButtonGUI{Texture: "infra/cobble_road", X: float64(50), Y: float64(InfraPanelTop), SX: 32, SY: 32},
+		b:  gui.ButtonGUI{Texture: "infra/cobble_road", X: float64(90), Y: float64(InfraPanelTop), SX: 32, SY: 32},
 		it: InfraTypeCobbleRoad,
 		ic: ic,
 	})
