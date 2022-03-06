e64cc68d0a651b6a801e4cd00407dd83b0f7b182
Gergely Svigruha <sgergo88@gmail.com>
2022-03-06 16:07:51 -0500

    Nits

--- view/field_renderer.go
+++ view/field_renderer.go
@@ -16,17 +16,13 @@ func RenderField(ic *ImageCache, cv *canvas.Canvas, rf renderer.RenderedField, f
 	units := f.Building.BuildingUnits
 	if len(units) > 0 {
 		for k := 0; k < len(units); k++ {
-			//rbu := RenderBuildingUnit(cv, &units[k], rf, k, c)
-			z := float64((k+1)*BuildingUnitHeight) * DZ
-			unitImg, rbu := ic.Bic.RenderBuildingUnitOnBuffer(&units[k], rf, k, c)
-			cv.DrawImage(unitImg, xMin, yMin-z, float64(unitImg.Width()), float64(unitImg.Height()))
+			unitImg, rbu, x, y := ic.Bic.RenderBuildingUnitOnBuffer(&units[k], rf, k, c)
+			cv.DrawImage(unitImg, x, y, float64(unitImg.Width()), float64(unitImg.Height()))
 			c.AddRenderedBuildingUnit(&rbu)
 		}
 		if f.Building.RoofUnit != nil {
-			numUnits := len(units)
-			z := float64((numUnits+1)*BuildingUnitHeight) * DZ
-			roofImg := ic.Bic.RenderBuildingRoofOnBuffer(f.Building.RoofUnit, rf, numUnits, c)
-			cv.DrawImage(roofImg, xMin, yMin-z, float64(roofImg.Width()), float64(roofImg.Height()))
+			roofImg, x, y := ic.Bic.RenderBuildingRoofOnBuffer(f.Building.RoofUnit, rf, len(units), c)
+			cv.DrawImage(roofImg, x, y, float64(roofImg.Width()), float64(roofImg.Height()))
 		}
 	}
 
