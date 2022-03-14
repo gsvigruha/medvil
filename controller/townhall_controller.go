480ae013dccc9927389aac60bf04dc3b0f117af1
Gergely Svigruha <sgergo88@gmail.com>
2022-03-13 22:20:59 -0400

    Fund markets

--- controller/townhall_controller.go
+++ controller/townhall_controller.go
@@ -21,5 +21,7 @@ func TownhallToControlPanel(cp *ControlPanel, th *social.Townhall) {
 	hp.AddPanel(gui.CreateNumberPanel(150, 625, 120, 20, 0, 1000, 10, "mine threshold %v", &th.Household.Town.Transfers.Mine.TaxThreshold).P)
 	hp.AddPanel(gui.CreateNumberPanel(150, 650, 120, 20, 0, 1000, 10, "mine subsidy %v", &th.Household.Town.Transfers.Mine.Subsidy).P)
 
+	hp.AddPanel(gui.CreateNumberPanel(150, 675, 120, 20, 0, 100, 10, "market funding %v", &th.Household.Town.Transfers.MarketFundingRate).P)
+
 	cp.SetDynamicPanel(hp)
 }
--- model/social/money_transfers.go
+++ model/social/money_transfers.go
@@ -7,9 +7,10 @@ type TransferCategories struct {
 }
 
 type MoneyTransfers struct {
-	Farm     TransferCategories
-	Workshop TransferCategories
-	Mine     TransferCategories
+	Farm              TransferCategories
+	Workshop          TransferCategories
+	Mine              TransferCategories
+	MarketFundingRate int
 }
 
 func (t *TransferCategories) Transfer(townMoney, householdMoney *uint32) {
@@ -22,6 +23,16 @@ func (t *TransferCategories) Transfer(townMoney, householdMoney *uint32) {
 		if *townMoney >= subsidy {
 			*householdMoney += subsidy
 			*townMoney -= subsidy
+		}
+	}
+}
+
+func (t *MoneyTransfers) FundMarket(townMoney, marketMoney *uint32) {
+	if int(*marketMoney) < int(*townMoney)*100/t.MarketFundingRate {
+		transfer := *townMoney*100/uint32(t.MarketFundingRate) - *marketMoney
+		if transfer <= *townMoney {
+			*townMoney -= transfer
+			*marketMoney += transfer
 		}
 	}
 }
