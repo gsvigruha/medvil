f628ae2fe2ac8f721def5020463753740c5d16f2
Gergely Svigruha <sgergo88@gmail.com>
2022-03-13 23:31:09 -0400

    Fixes

--- model/social/workshop.go
+++ model/social/workshop.go
@@ -22,18 +22,22 @@ func (w *Workshop) ElapseTime(Calendar *time.CalendarType, m navigation.IMap) {
 				needs := w.Household.Resources.Needs(a.Multiply(ProductTransportQuantity).Wrap())
 				tag := "manufacture_input#" + a.A.Name
 				if needs != nil && w.Household.NumTasks("exchange", tag) == 0 {
-					if mp.Storage.Has(needs) && w.Household.Money >= mp.Price(needs) {
-						mx, my := mp.Building.GetRandomBuildingXY()
-						w.Household.AddTask(&economy.ExchangeTask{
-							HomeF:          home,
-							MarketF:        m.GetField(mx, my),
-							Exchange:       mp,
-							HouseholdR:     &w.Household.Resources,
-							HouseholdMoney: &w.Household.Money,
-							GoodsToBuy:     needs,
-							GoodsToSell:    nil,
-							TaskTag:        tag,
-						})
+					if w.Household.Money >= mp.Price(needs) {
+						if mp.Storage.Has(needs) {
+							mx, my := mp.Building.GetRandomBuildingXY()
+							w.Household.AddTask(&economy.ExchangeTask{
+								HomeF:          home,
+								MarketF:        m.GetField(mx, my),
+								Exchange:       mp,
+								HouseholdR:     &w.Household.Resources,
+								HouseholdMoney: &w.Household.Money,
+								GoodsToBuy:     needs,
+								GoodsToSell:    nil,
+								TaskTag:        tag,
+							})
+						} else if Calendar.Day == 30 && Calendar.Hour == 0 {
+							mp.RegisterDemand(needs)
+						}
 					}
 				}
 			}
