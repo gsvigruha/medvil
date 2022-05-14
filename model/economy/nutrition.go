f92071798e47e7b24edbed22de25cdd8ed8f7e10
Gergely Svigruha <sgergo88@gmail.com>
2022-05-14 15:29:47 -0400

    Nit

--- model/economy/nutrition.go
+++ model/economy/nutrition.go
@@ -5,19 +5,19 @@ import (
 	"medvil/model/navigation"
 )
 
-var fruit = artifacts.GetArtifact("fruit")
-var vegetable = artifacts.GetArtifact("vegetable")
-var bread = artifacts.GetArtifact("bread")
-var meat = artifacts.GetArtifact("meat")
+var Fruit = artifacts.GetArtifact("fruit")
+var Vegetable = artifacts.GetArtifact("vegetable")
+var Bread = artifacts.GetArtifact("bread")
+var Meat = artifacts.GetArtifact("meat")
 
-var water = artifacts.GetArtifact("water")
-var wine = artifacts.GetArtifact("wine")
+var Water = artifacts.GetArtifact("water")
+var Wine = artifacts.GetArtifact("wine")
 var Beer = artifacts.GetArtifact("beer")
 
 var Medicine = artifacts.GetArtifact("medicine")
 
-var Foods = []*artifacts.Artifact{fruit, vegetable, bread, meat}
-var Drinks = []*artifacts.Artifact{water, wine, Beer}
+var Foods = []*artifacts.Artifact{Fruit, Vegetable, Bread, Meat}
+var Drinks = []*artifacts.Artifact{Water, Wine, Beer}
 
 const MinFoodOrDrinkPerPerson uint16 = 2
 const MaxFoodOrDrinkPerPerson uint16 = 5
@@ -95,12 +95,12 @@ type PersonStateChange struct {
 }
 
 var ArtifactToPersonState = map[*artifacts.Artifact]PersonStateChange{
-	fruit:     PersonStateChange{Food: 50, Water: 50, Happiness: 25, Health: 25},
-	vegetable: PersonStateChange{Food: 100, Water: 0, Happiness: 0, Health: 50},
-	bread:     PersonStateChange{Food: 200, Water: 0, Happiness: 0, Health: 0},
-	meat:      PersonStateChange{Food: 200, Water: 0, Happiness: 50, Health: 0},
-	water:     PersonStateChange{Food: 0, Water: 200, Happiness: 0, Health: 0},
-	wine:      PersonStateChange{Food: 0, Water: 150, Happiness: 100, Health: 50},
+	Fruit:     PersonStateChange{Food: 50, Water: 50, Happiness: 25, Health: 25},
+	Vegetable: PersonStateChange{Food: 100, Water: 0, Happiness: 0, Health: 50},
+	Bread:     PersonStateChange{Food: 200, Water: 0, Happiness: 0, Health: 0},
+	Meat:      PersonStateChange{Food: 200, Water: 0, Happiness: 50, Health: 0},
+	Water:     PersonStateChange{Food: 0, Water: 200, Happiness: 0, Health: 0},
+	Wine:      PersonStateChange{Food: 0, Water: 150, Happiness: 100, Health: 50},
 	Beer:      PersonStateChange{Food: 25, Water: 150, Happiness: 100, Health: 0},
 	Medicine:  PersonStateChange{Food: 0, Water: 0, Happiness: 0, Health: 150},
 }
