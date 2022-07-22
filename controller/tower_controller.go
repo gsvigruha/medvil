909b9ac3cc9919b05424f934328a1f9962489d79
Gergely Svigruha <sgergo88@gmail.com>
2022-07-21 21:23:43 -0400

    Allocate patrol land to tower

--- controller/tower_controller.go
+++ controller/tower_controller.go
@@ -2,7 +2,10 @@ package controller
 
 import (
 	"github.com/tfriedel6/canvas"
+	"medvil/model/military"
+	"medvil/model/navigation"
 	"medvil/model/social"
+	"medvil/renderer"
 	"medvil/view/gui"
 )
 
@@ -10,6 +13,15 @@ type TowerController struct {
 	householdPanel *gui.Panel
 	towerPanel     *gui.Panel
 	tower          *social.Tower
+	UseType        uint8
+}
+
+func (tc *TowerController) GetUseType() uint8 {
+	return tc.UseType
+}
+
+func (tc *TowerController) SetUseType(ut uint8) {
+	tc.UseType = ut
 }
 
 func TowerToControlPanel(cp *ControlPanel, tower *social.Tower) {
@@ -17,8 +29,21 @@ func TowerToControlPanel(cp *ControlPanel, tower *social.Tower) {
 	tp := &gui.Panel{X: 0, Y: ControlPanelDynamicPanelTop + HouseholdControllerSY, SX: ControlPanelSX, SY: ControlPanelDynamicPanelSY - HouseholdControllerSY}
 	HouseholdToControlPanel(hp, &tower.Household)
 	tc := &TowerController{towerPanel: tp, householdPanel: hp, tower: tower}
+	tc.UseType = military.MilitaryLandUseTypeNone
+
+	tp.AddButton(LandUseButton{
+		b:       gui.ButtonGUI{Texture: "terrain/grass", X: float64(10), Y: float64(HouseholdControllerGUIBottomY), SX: 32, SY: 32},
+		luc:     tc,
+		useType: military.MilitaryLandUseTypeNone,
+	})
+	tp.AddButton(LandUseButton{
+		b:       gui.ButtonGUI{Texture: "terrain/grass", X: float64(40), Y: float64(HouseholdControllerGUIBottomY), SX: 32, SY: 32},
+		luc:     tc,
+		useType: military.MilitaryLandUseTypePatrol,
+	})
 
 	cp.SetDynamicPanel(tc)
+	cp.C.ClickHandler = tc
 }
 
 func (tc *TowerController) CaptureClick(x, y float64) {
@@ -36,4 +61,32 @@ func (tc *TowerController) Clear() {}
 func (tc *TowerController) Refresh() {
 	tc.householdPanel.Clear()
 	HouseholdToControlPanel(tc.householdPanel, &tc.tower.Household)
+}
+
+func (tc *TowerController) GetActiveFields(c *Controller, rf *renderer.RenderedField) []navigation.FieldWithContext {
+	return tc.tower.GetFields()
+}
+
+func (tc *TowerController) HandleClick(c *Controller, rf *renderer.RenderedField) bool {
+	for i := range tc.tower.Land {
+		l := &tc.tower.Land[i]
+		if l.F.X == rf.F.X && l.F.Y == rf.F.Y {
+			if tc.UseType == military.MilitaryLandUseTypeNone {
+				// Disallocate land
+				tc.tower.Land = append(tc.tower.Land[:i], tc.tower.Land[i+1:]...)
+				rf.F.Allocated = false
+			}
+			return true
+		}
+	}
+	if !rf.F.Allocated && tc.UseType == military.MilitaryLandUseTypePatrol {
+		tc.tower.Land = append(tc.tower.Land, social.PatrolLand{
+			X: rf.F.X,
+			Y: rf.F.Y,
+			F: rf.F,
+		})
+		rf.F.Allocated = true
+		return true
+	}
+	return false
 }
--- model/social/tower.go
+++ model/social/tower.go
@@ -10,7 +10,23 @@ type PatrolLand struct {
 	F *navigation.Field
 }
 
+func (l PatrolLand) Field() *navigation.Field {
+	return l.F
+}
+
+func (l PatrolLand) Context() string {
+	return "shield"
+}
+
 type Tower struct {
 	Household Household
 	Land      []PatrolLand
+}
+
+func (t *Tower) GetFields() []navigation.FieldWithContext {
+	fields := make([]navigation.FieldWithContext, len(t.Land))
+	for i := range t.Land {
+		fields[i] = t.Land[i]
+	}
+	return fields
 }
