4d9cf2debc82262a61d267dcaa93a0d8209a7285
Gergely Svigruha <gsvigruha@users.noreply.github.com>
2022-08-13 17:01:02 -0400

    Wall shapes / textures (#185)
    * Add ftone with flowers
    
    * Add ivy
    
    * Wall shape
    
    * Generate shapes

---
+++ model/building/building_shape.go
@@ -0,0 +1,22 @@
+package building
+
+import (
+	"math/rand"
+)
+
+const NumShapes = 5
+
+const ShapeAreaSize = 3
+
+func GetShape(t BuildingType, x, y uint16) uint8 {
+	if t == BuildingTypeWall {
+		return WallShape(x, y)
+	} else if t == BuildingTypeWorkshop {
+		return uint8(rand.Intn(NumShapes))
+	}
+	return 0
+}
+
+func WallShape(x, y uint16) uint8 {
+	return uint8((13*(x/ShapeAreaSize) + 7*(y/ShapeAreaSize)) % NumShapes)
+}
