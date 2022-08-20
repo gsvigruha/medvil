78ee98121ddd6a7712a2aeecf560dddf464a6f5a
Gergely Svigruha <sgergo88@gmail.com>
2022-08-19 20:55:13 -0400

    Trader

--- model/economy/trade_task.go
+++ model/economy/trade_task.go
@@ -7,15 +7,19 @@ import (
 	"medvil/model/vehicles"
 )
 
+const TradeTaskStatePickup uint8 = 0
+const TradeTaskStateDrop uint8 = 1
+
 type TradeTask struct {
 	TaskBase
-	MarketF        *navigation.Field
-	Exchange       Exchange
+	SourceMarketF  *navigation.Field
+	TargetMarketF  *navigation.Field
+	SourceExchange Exchange
+	TargetExchange Exchange
 	TraderR        *artifacts.Resources
 	TraderMoney    *uint32
 	Vehicle        *vehicles.Vehicle
-	GoodsToBuy     []artifacts.Artifacts
-	GoodsToSell    []artifacts.Artifacts
+	GoodsToTrade   []artifacts.Artifacts
 	TaskTag        string
 	goods          []artifacts.Artifacts
 	state          uint8
@@ -23,15 +27,45 @@ type TradeTask struct {
 }
 
 func (t *TradeTask) Field() *navigation.Field {
-	return t.MarketF
+	switch t.state {
+	case TradeTaskStatePickup:
+		return t.SourceMarketF
+	case TradeTaskStateDrop:
+		return t.TargetMarketF
+	}
+	return nil
 }
 
 func (t *TradeTask) Complete(Calendar *time.CalendarType, tool bool) bool {
+	switch t.state {
+	case TradeTaskStatePickup:
+		t.goods = t.SourceExchange.BuyAsManyAsPossible(t.GoodsToTrade, t.TraderMoney)
+		t.state = TradeTaskStateDrop
+	case TradeTaskStateDrop:
+		if t.TargetExchange.CanSell(t.goods) {
+			t.TargetExchange.Sell(t.goods, t.TraderMoney)
+			return true
+		} else {
+			t.waittime++
+		}
+		if t.waittime > MaxWaitTime {
+			if t.TargetExchange.CanSell(t.goods) {
+				t.TargetExchange.Sell(t.goods, t.TraderMoney)
+			}
+			return true
+		}
+	}
 	return false
 }
 
 func (t *TradeTask) Blocked() bool {
-	return !t.Exchange.CanSell(t.goods) || t.Exchange.Price(t.GoodsToBuy) > *t.TraderMoney || !t.Exchange.CanBuy(t.GoodsToBuy)
+	switch t.state {
+	case TradeTaskStatePickup:
+		return !t.SourceExchange.CanSell(t.GoodsToTrade) || t.SourceExchange.Price(t.GoodsToTrade) > *t.TraderMoney
+	case TradeTaskStateDrop:
+		return false
+	}
+	return false
 }
 
 func (t *TradeTask) Name() string {
