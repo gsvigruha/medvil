da6f173b9c75c7ef76de087764becc22efdbd6df
Gergely Svigruha <gergelysvigruha@Gergelys-MBP.home>
2022-09-17 10:33:08 -0400

    Refactor trading

--- model/economy/trade_task.go
+++ model/economy/trade_task.go
@@ -7,43 +7,65 @@ import (
 	"medvil/model/vehicles"
 )
 
-const TradeTaskStatePickup uint8 = 0
-const TradeTaskStateDrop uint8 = 1
+const TradeTaskStatePickupAtSource uint8 = 0
+const TradeTaskStateDropoffAtDest uint8 = 1
+const TradeTaskStatePickupAtDest uint8 = 2
+const TradeTaskStateDropoffAtSource uint8 = 3
 
 type TradeTask struct {
 	TaskBase
-	SourceMarketF  *navigation.Field
-	TargetMarketF  *navigation.Field
-	SourceExchange Exchange
-	TargetExchange Exchange
-	TraderR        *artifacts.Resources
-	TraderMoney    *uint32
-	Vehicle        *vehicles.Vehicle
-	GoodsToTrade   []artifacts.Artifacts
-	TaskTag        string
-	goods          []artifacts.Artifacts
-	state          uint8
-	waittime       uint16
+	SourceMarketF     *navigation.Field
+	TargetMarketF     *navigation.Field
+	SourceExchange    Exchange
+	TargetExchange    Exchange
+	TraderR           *artifacts.Resources
+	TraderMoney       *uint32
+	Vehicle           *vehicles.Vehicle
+	GoodsSourceToDest []artifacts.Artifacts
+	GoodsDestToSource []artifacts.Artifacts
+	TaskTag           string
+	goods             []artifacts.Artifacts
+	state             uint8
+	waittime          uint16
 }
 
 func (t *TradeTask) Field() *navigation.Field {
 	switch t.state {
-	case TradeTaskStatePickup:
+	case TradeTaskStatePickupAtSource:
 		return t.SourceMarketF
-	case TradeTaskStateDrop:
+	case TradeTaskStateDropoffAtDest:
 		return t.TargetMarketF
+	case TradeTaskStatePickupAtDest:
+		return t.TargetMarketF
+	case TradeTaskStateDropoffAtSource:
+		return t.SourceMarketF
 	}
 	return nil
 }
 
 func (t *TradeTask) Complete(Calendar *time.CalendarType, tool bool) bool {
 	switch t.state {
-	case TradeTaskStatePickup:
-		t.goods = t.SourceExchange.BuyAsManyAsPossible(t.GoodsToTrade, t.TraderMoney)
-		t.state = TradeTaskStateDrop
-	case TradeTaskStateDrop:
+	case TradeTaskStatePickupAtSource:
+		t.goods = t.SourceExchange.BuyAsManyAsPossible(t.GoodsSourceToDest, t.TraderMoney)
+		t.state = TradeTaskStateDropoffAtDest
+		t.waittime = 0
+	case TradeTaskStateDropoffAtDest:
 		if t.TargetExchange.CanSell(t.goods) {
 			t.TargetExchange.Sell(t.goods, t.TraderMoney)
+			t.state = TradeTaskStatePickupAtDest
+		} else {
+			t.waittime++
+		}
+		if t.waittime > MaxWaitTime {
+			t.state = TradeTaskStatePickupAtDest
+		}
+	case TradeTaskStatePickupAtDest:
+		t.goods = t.TargetExchange.BuyAsManyAsPossible(t.GoodsDestToSource, t.TraderMoney)
+		t.state = TradeTaskStateDropoffAtSource
+		t.waittime = 0
+	case TradeTaskStateDropoffAtSource:
+		if t.SourceExchange.CanSell(t.goods) {
+			t.SourceExchange.Sell(t.goods, t.TraderMoney)
 			return true
 		} else {
 			t.waittime++
@@ -56,12 +78,6 @@ func (t *TradeTask) Complete(Calendar *time.CalendarType, tool bool) bool {
 }
 
 func (t *TradeTask) Blocked() bool {
-	switch t.state {
-	case TradeTaskStatePickup:
-		return !t.SourceExchange.CanSell(t.GoodsToTrade) || t.SourceExchange.Price(t.GoodsToTrade) > *t.TraderMoney
-	case TradeTaskStateDrop:
-		return false
-	}
 	return false
 }
 
--- model/social/trading.go
+++ model/social/trading.go
@@ -34,37 +34,53 @@ func (t *Trader) ElapseTime(Calendar *time.CalendarType, m navigation.IMap) {
 	}
 }
 
-func (t *Trader) GetTradeTask(m navigation.IMap) *economy.TradeTask {
-	if t.TargetExchange == nil {
-		return nil
-	}
+func (t *Trader) GetArtifactToTrade(pickupMP, dropoffMP *Marketplace) *artifacts.Artifact {
 	var weights []float64
 	var as []*artifacts.Artifact
 	for _, a := range artifacts.All {
-		if t.SourceExchange.HasTraded(a) && t.TargetExchange.HasTraded(a) {
-			if t.SourceExchange.Prices[a]*TradeProfitThreshold <= t.TargetExchange.Prices[a] {
-				profit := float64(t.TargetExchange.Prices[a]) / float64(t.SourceExchange.Prices[a])
+		if pickupMP.HasTraded(a) && dropoffMP.HasTraded(a) {
+			if pickupMP.Prices[a]*TradeProfitThreshold <= dropoffMP.Prices[a] {
+				profit := float64(dropoffMP.Prices[a]) / float64(pickupMP.Prices[a])
 				weights = append(weights, profit)
 				as = append(as, a)
 			}
 		}
 	}
 	if len(weights) > 0 {
-		artifactToTrade := as[util.RandomIndexWeighted(weights)]
+		return as[util.RandomIndexWeighted(weights)]
+	}
+	return nil
+}
+
+func (t *Trader) GetGoodsToTrade(a *artifacts.Artifact, mp *Marketplace) []artifacts.Artifacts {
+	if a != nil {
+		quantity := uint16(t.Money / mp.Prices[a])
+		return []artifacts.Artifacts{artifacts.Artifacts{A: a, Quantity: quantity}}
+	}
+	return []artifacts.Artifacts{}
+}
+
+func (t *Trader) GetTradeTask(m navigation.IMap) *economy.TradeTask {
+	if t.TargetExchange == nil {
+		return nil
+	}
+	artifactSourceToDest := t.GetArtifactToTrade(t.SourceExchange, t.TargetExchange)
+	artifactDestToSource := t.GetArtifactToTrade(t.SourceExchange, t.TargetExchange)
+	if artifactSourceToDest != nil || artifactDestToSource != nil {
 		smx, smy, smok := GetRandomBuildingXY(t.SourceExchange.Building, m, navigation.Field.BuildingNonExtension)
 		tmx, tmy, tmok := GetRandomBuildingXY(t.TargetExchange.Building, m, navigation.Field.BuildingNonExtension)
 		if smok && tmok {
-			quantity := uint16(t.Money / t.SourceExchange.Prices[artifactToTrade])
 			return &economy.TradeTask{
-				SourceMarketF:  m.GetField(smx, smy),
-				TargetMarketF:  m.GetField(tmx, tmy),
-				SourceExchange: t.SourceExchange,
-				TargetExchange: t.TargetExchange,
-				TraderR:        &t.Resources,
-				TraderMoney:    &t.Money,
-				Vehicle:        nil,
-				GoodsToTrade:   []artifacts.Artifacts{artifacts.Artifacts{A: artifactToTrade, Quantity: quantity}},
-				TaskTag:        "",
+				SourceMarketF:     m.GetField(smx, smy),
+				TargetMarketF:     m.GetField(tmx, tmy),
+				SourceExchange:    t.SourceExchange,
+				TargetExchange:    t.TargetExchange,
+				TraderR:           &t.Resources,
+				TraderMoney:       &t.Money,
+				Vehicle:           nil,
+				GoodsSourceToDest: t.GetGoodsToTrade(artifactSourceToDest, t.SourceExchange),
+				GoodsDestToSource: t.GetGoodsToTrade(artifactDestToSource, t.TargetExchange),
+				TaskTag:           "",
 			}
 		}
 	}
