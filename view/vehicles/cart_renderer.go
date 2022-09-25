dda17e8a936f2ab7add3fb1c79fc63fe276eebe8
Gergely Svigruha <sgergo88@gmail.com>
2022-09-25 14:30:01 -0400

    For real

---
+++ view/vehicles/cart_renderer.go
@@ -0,0 +1,128 @@
+package vehicles
+
+import (
+	//"fmt"
+	"github.com/tfriedel6/canvas"
+	"math"
+	"medvil/controller"
+	"medvil/model/navigation"
+	"medvil/view/animation"
+)
+
+func DrawTradingCart(cv *canvas.Canvas, t *navigation.Traveller, x float64, y float64, c *controller.Controller) {
+	f1, f2, z, _, h2 := DrawCart(cv, t, x, y, c)
+
+	dirIdx := (c.Perspective - t.Direction) % 4
+	pm := animation.ProjectionMatrices[dirIdx]
+
+	h3 := 21.0
+
+	cv.SetStrokeStyle("#321")
+	cv.SetLineWidth(1)
+
+	cv.BeginPath()
+	cv.LineTo(x+f1*pm.XX-h2*pm.XY-z*pm.XZ, y+f1*pm.YX-h2*pm.YY-z*pm.YZ)
+	cv.LineTo(x+f1*pm.XX-h3*pm.XY-z*pm.XZ, y+f1*pm.YX-h3*pm.YY-z*pm.YZ)
+	cv.ClosePath()
+	cv.Stroke()
+
+	cv.BeginPath()
+	cv.LineTo(x+f1*pm.XX-h2*pm.XY+z*pm.XZ, y+f1*pm.YX-h2*pm.YY+z*pm.YZ)
+	cv.LineTo(x+f1*pm.XX-h3*pm.XY+z*pm.XZ, y+f1*pm.YX-h3*pm.YY+z*pm.YZ)
+	cv.ClosePath()
+	cv.Stroke()
+
+	cv.BeginPath()
+	cv.LineTo(x+f2*pm.XX-h2*pm.XY+z*pm.XZ, y+f2*pm.YX-h2*pm.YY+z*pm.YZ)
+	cv.LineTo(x+f2*pm.XX-h3*pm.XY+z*pm.XZ, y+f2*pm.YX-h3*pm.YY+z*pm.YZ)
+	cv.ClosePath()
+	cv.Stroke()
+
+	cv.BeginPath()
+	cv.LineTo(x+f2*pm.XX-h2*pm.XY-z*pm.XZ, y+f2*pm.YX-h2*pm.YY-z*pm.YZ)
+	cv.LineTo(x+f2*pm.XX-h3*pm.XY-z*pm.XZ, y+f2*pm.YX-h3*pm.YY-z*pm.YZ)
+	cv.ClosePath()
+	cv.Stroke()
+
+	if dirIdx == 1 || dirIdx == 3 {
+		cv.SetFillStyle("texture/vehicle/textile.png")
+	} else {
+		cv.SetFillStyle("texture/vehicle/textile_flipped.png")
+	}
+	cv.BeginPath()
+	cv.LineTo(x+f1*pm.XX-h3*pm.XY-z*pm.XZ, y+f1*pm.YX-h3*pm.YY-z*pm.YZ)
+	cv.LineTo(x+f1*pm.XX-h3*pm.XY+z*pm.XZ, y+f1*pm.YX-h3*pm.YY+z*pm.YZ)
+	cv.LineTo(x+f2*pm.XX-h3*pm.XY+z*pm.XZ, y+f2*pm.YX-h3*pm.YY+z*pm.YZ)
+	cv.LineTo(x+f2*pm.XX-h3*pm.XY-z*pm.XZ, y+f2*pm.YX-h3*pm.YY-z*pm.YZ)
+	cv.ClosePath()
+	cv.Fill()
+}
+
+func DrawCart(cv *canvas.Canvas, t *navigation.Traveller, x float64, y float64, c *controller.Controller) (float64, float64, float64, float64, float64) {
+	if !t.Visible {
+		return 0, 0, 0, 0, 0
+	}
+	dirIdx := (c.Perspective - t.Direction) % 4
+	pm := animation.ProjectionMatrices[dirIdx]
+	var r = 8.0 - float64(t.Phase%16)
+	if r < 0.0 {
+		r = -r
+	}
+	var dir = 1.0
+	if dirIdx == 0 || dirIdx == 1 {
+		dir = -1.0
+	}
+
+	f1 := 3.0
+	f2 := 17.0
+	z := 6.0
+	h1 := 8.0
+	h2 := 12.0
+
+	cv.SetFillStyle("texture/vehicle/boat_bottom.png")
+	cv.SetStrokeStyle("#321")
+	cv.SetLineWidth(1)
+	cv.BeginPath()
+	for i := 0.0; i < 8; i++ {
+		dx0 := math.Cos(math.Pi*2.0*i/8.0)*(h1/2.0) + (f2-f1)/2.0
+		dy0 := math.Sin(math.Pi*2.0*i/8.0)*(h1/2.0) + h1/2.0
+		cv.LineTo(x+dx0*pm.XX-dy0*pm.XY-z*pm.XZ*dir, y+dx0*pm.YX-dy0*pm.YY-z*pm.YZ*dir)
+	}
+	cv.ClosePath()
+	cv.Fill()
+	cv.Stroke()
+
+	cv.SetFillStyle("texture/vehicle/boat_bottom.png")
+	cv.BeginPath()
+	cv.LineTo(x+f1*pm.XX-h1*pm.XY-z*pm.XZ, y+f1*pm.YX-h1*pm.YY-z*pm.YZ)
+	cv.LineTo(x+f1*pm.XX-h1*pm.XY+z*pm.XZ, y+f1*pm.YX-h1*pm.YY+z*pm.YZ)
+	cv.LineTo(x+f2*pm.XX-h1*pm.XY+z*pm.XZ, y+f2*pm.YX-h1*pm.YY+z*pm.YZ)
+	cv.LineTo(x+f2*pm.XX-h1*pm.XY-z*pm.XZ, y+f2*pm.YX-h1*pm.YY-z*pm.YZ)
+	cv.ClosePath()
+	cv.Fill()
+
+	cv.SetStrokeStyle("#321")
+	cv.SetLineWidth(2)
+	cv.BeginPath()
+	cv.LineTo(x+f1*pm.XX-h2*pm.XY-z*pm.XZ, y+f1*pm.YX-h2*pm.YY-z*pm.YZ)
+	cv.LineTo(x+f1*pm.XX-h2*pm.XY+z*pm.XZ, y+f1*pm.YX-h2*pm.YY+z*pm.YZ)
+	cv.LineTo(x+f2*pm.XX-h2*pm.XY+z*pm.XZ, y+f2*pm.YX-h2*pm.YY+z*pm.YZ)
+	cv.LineTo(x+f2*pm.XX-h2*pm.XY-z*pm.XZ, y+f2*pm.YX-h2*pm.YY-z*pm.YZ)
+	cv.ClosePath()
+	cv.Stroke()
+
+	cv.SetFillStyle("texture/vehicle/boat_bottom.png")
+	cv.SetStrokeStyle("#321")
+	cv.SetLineWidth(1)
+	cv.BeginPath()
+	for i := 0.0; i < 8; i++ {
+		dx0 := math.Cos(math.Pi*2.0*i/8.0)*(h1/2.0) + (f2-f1)/2.0 + f1
+		dy0 := math.Sin(math.Pi*2.0*i/8.0)*(h1/2.0) + h1/2.0
+		cv.LineTo(x+dx0*pm.XX-dy0*pm.XY+z*pm.XZ*dir, y+dx0*pm.YX-dy0*pm.YY+z*pm.YZ*dir)
+	}
+	cv.ClosePath()
+	cv.Fill()
+	cv.Stroke()
+
+	return f1, f2, z, h1, h2
+}
