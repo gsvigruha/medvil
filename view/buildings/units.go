43ba807ad7f1cf031ad9b1590850357da4caee1a
Gergely Svigruha <gsvigruha@users.noreply.github.com>
2023-04-08 21:47:08 -0400

    Render farm ornaments (#258)

--- view/buildings/units.go
+++ view/buildings/units.go
@@ -100,14 +100,39 @@ func RenderBuildingUnit(cv *canvas.Canvas, unit *building.BuildingUnit, rf rende
 			cv.Stroke()
 
 			if !wall.Arch && !unit.Construction {
-				cv.SetFillStyle("texture/building/ornament.png")
-				cv.BeginPath()
-				cv.LineTo(rw.X[1], rw.Y[1])
-				cv.LineTo(rw.X[2], rw.Y[2])
-				cv.LineTo(rw.X[2], rw.Y[2]+5)
-				cv.LineTo(rw.X[1], rw.Y[1]+5)
-				cv.ClosePath()
-				cv.Fill()
+				if unit.B.Plan.BuildingType == building.BuildingTypeWorkshop || unit.B.Plan.BuildingType == building.BuildingTypeTownhall {
+					cv.SetFillStyle("texture/building/ornament.png")
+					cv.BeginPath()
+					cv.LineTo(rw.X[1], rw.Y[1])
+					cv.LineTo(rw.X[2], rw.Y[2])
+					cv.LineTo(rw.X[2], rw.Y[2]+5)
+					cv.LineTo(rw.X[1], rw.Y[1]+5)
+					cv.ClosePath()
+					cv.Fill()
+				} else if unit.B.Plan.BuildingType == building.BuildingTypeFarm {
+					cv.SetFillStyle("texture/building/ornament_wood.png")
+					cv.BeginPath()
+					cv.LineTo(rw.X[1], rw.Y[1])
+					cv.LineTo(rw.X[2], rw.Y[2])
+					cv.LineTo(rw.X[2], rw.Y[2]+5)
+					cv.LineTo(rw.X[1], rw.Y[1]+5)
+					cv.ClosePath()
+					cv.Fill()
+					cv.BeginPath()
+					cv.LineTo(rw.X[1], rw.Y[1]*0.7+rw.Y[0]*0.3)
+					cv.LineTo(rw.X[1], rw.Y[1]*0.6+rw.Y[0]*0.4)
+					cv.LineTo(rw.X[1]*0.7+rw.X[2]*0.3, rw.Y[1]*0.7+rw.Y[2]*0.3)
+					cv.LineTo(rw.X[1]*0.8+rw.X[2]*0.2, rw.Y[1]*0.8+rw.Y[2]*0.2)
+					cv.ClosePath()
+					cv.Fill()
+					cv.BeginPath()
+					cv.LineTo(rw.X[2], rw.Y[2]*0.7+rw.Y[3]*0.3)
+					cv.LineTo(rw.X[2], rw.Y[2]*0.6+rw.Y[3]*0.4)
+					cv.LineTo(rw.X[2]*0.7+rw.X[1]*0.3, rw.Y[2]*0.7+rw.Y[1]*0.3)
+					cv.LineTo(rw.X[2]*0.8+rw.X[1]*0.2, rw.Y[2]*0.8+rw.Y[1]*0.2)
+					cv.ClosePath()
+					cv.Fill()
+				}
 			}
 
 			/*
