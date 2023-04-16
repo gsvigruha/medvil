1e6819bd1c21e2075c6af177210b1e5e04e0dcea
Gergely Svigruha <sgergo88@gmail.com>
2023-04-16 00:28:42 -0400

    Nit

--- model/economy/transport.go
+++ model/economy/transport.go
@@ -31,11 +31,11 @@ func (t *TransportTask) Destination() navigation.Destination {
 func (t *TransportTask) Complete(Calendar *time.CalendarType, tool bool) bool {
 	if t.dropoff {
 		t.DropoffR.Add(t.A, t.q)
-		finished := t.Quantity == 0 || !t.CompleteQuantity
-		if !finished {
-			t.dropoff = false
+		if t.Quantity == 0 || !t.CompleteQuantity {
+			return true
 		}
-		return finished
+		t.dropoff = false
+		return false
 	} else {
 		t.q = t.PickupR.Remove(t.A, t.Quantity)
 		t.Quantity -= t.q
