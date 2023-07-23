d6e33c02307441ffd480932bc06fbaa89cd0e2eb
Gergely Svigruha <gsvigruha@users.noreply.github.com>
2023-07-23 11:36:22 -0400

    More efficient tree buffer (#288)

--- view/plant_buffer.go
+++ view/plant_buffer.go
@@ -7,6 +7,7 @@ import (
 	"medvil/controller"
 	"medvil/model/terrain"
 	"medvil/renderer"
+	"strconv"
 	"time"
 )
 
@@ -24,30 +25,40 @@ func getPlantBufferSize(p *terrain.Plant) (float64, float64) {
 }
 
 type PlantImageCache struct {
-	entries map[*terrain.Plant]*CacheEntry
+	entries map[string]*CacheEntry
 	ctx     *goglbackend.GLContext
 }
 
 func (ic *PlantImageCache) RenderPlantOnBuffer(p *terrain.Plant, rf renderer.RenderedField, c *controller.Controller) *canvas.Canvas {
+	keyByShape := p.CacheKey() + "#" + strconv.Itoa(int(c.Map.Calendar.Month)) + "#" + strconv.Itoa(int(c.Map.Calendar.Day))
+	keyByLoc := strconv.Itoa(int(rf.F.X)) + "#" + strconv.Itoa(int(rf.F.Y))
 	t := time.Now().UnixNano()
+	nt := t - int64(rand.Intn(PlantRenderBufferTimeMs/2)*1000*1000) + int64(PlantRenderBufferTimeMs/4*1000*1000)
 	plantBufferW, plantBufferH := getPlantBufferSize(p)
-	if ce, ok := ic.entries[p]; ok {
+	if ce, ok := ic.entries[keyByShape]; ok && p.IsMature(c.Map.Calendar) {
+		ic.entries[keyByLoc] = ce
+		return ce.cv
+	}
+	if ce, ok := ic.entries[keyByLoc]; ok {
 		if t-ce.createdTime > int64(PlantRenderBufferTimeMs)*1000*1000 {
 			ce.cv.ClearRect(0, 0, plantBufferW, plantBufferH)
 			RenderPlant(ce.cv, p, rf, c)
-			ce.createdTime = t - int64(rand.Intn(PlantRenderBufferTimeMs/2)*1000*1000) + int64(PlantRenderBufferTimeMs/4*1000*1000)
+			ce.createdTime = nt
 		}
 		return ce.cv
-	} else {
-		offscreen, _ := goglbackend.NewOffscreen(int(plantBufferW), int(plantBufferH), true, ic.ctx)
-		cv := canvas.New(offscreen)
-		cv.ClearRect(0, 0, plantBufferW, plantBufferH)
-		RenderPlant(cv, p, rf, c)
-		ic.entries[p] = &CacheEntry{
-			offscreen:   offscreen,
-			cv:          cv,
-			createdTime: t - int64(rand.Intn(PlantRenderBufferTimeMs/2)*1000*1000) + int64(PlantRenderBufferTimeMs/4*1000*1000),
-		}
-		return cv
 	}
+	offscreen, _ := goglbackend.NewOffscreen(int(plantBufferW), int(plantBufferH), true, ic.ctx)
+	cv := canvas.New(offscreen)
+	cv.ClearRect(0, 0, plantBufferW, plantBufferH)
+	RenderPlant(cv, p, rf, c)
+	e := &CacheEntry{
+		offscreen:   offscreen,
+		cv:          cv,
+		createdTime: nt,
+	}
+	if p.IsMature(c.Map.Calendar) {
+		ic.entries[keyByShape] = e
+	}
+	ic.entries[keyByLoc] = e
+	return cv
 }
