b48a3340af8139cbda3d638620556517fa9ab1dc
Gergely Svigruha <sgergo88@gmail.com>
2023-08-12 15:15:52 -0400

    fix

--- model/social/household.go
+++ model/social/household.go
@@ -15,7 +15,7 @@ const ReproductionRate = 1.0 / (24 * 30 * 12)
 const ClothesConsumptionRate = 1.0 / (24 * 30 * 12 * 5)
 const StoragePerArea = 100
 const ExtrasBudgetRatio = 0.25
-const BuildingBrokenRate = 1.0 / (24 * 30 * 12 * 1)
+const BuildingBrokenRate = 1.0 / (24 * 30 * 12 * 10)
 
 var Log = artifacts.GetArtifact("log")
 var Firewood = artifacts.GetArtifact("firewood")
