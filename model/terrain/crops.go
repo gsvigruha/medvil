ee9babcfe872a6ac234b4d0b3c162ea979b3ec0c
Gergely Svigruha <sgergo88@gmail.com>
2023-09-03 11:37:43 -0400

    Fixes

--- model/terrain/crops.go
+++ model/terrain/crops.go
@@ -5,8 +5,8 @@ import (
 )
 
 var AllCropTypes = [...]*PlantType{
-	&PlantType{Name: "grain", MaturityAgeYears: 1, Yield: artifacts.Artifacts{A: artifacts.GetArtifact("grain"), Quantity: 2}, Tall: true, Habitat: Cultivated},
-	&PlantType{Name: "vegetables", MaturityAgeYears: 1, Yield: artifacts.Artifacts{A: artifacts.GetArtifact("vegetable"), Quantity: 4}, Tall: false, Habitat: Cultivated},
+	&PlantType{Name: "grain", MaturityAgeYears: 1, Yield: artifacts.Artifacts{A: artifacts.GetArtifact("grain"), Quantity: 2}, Tall: true, Habitat: Land},
+	&PlantType{Name: "vegetables", MaturityAgeYears: 1, Yield: artifacts.Artifacts{A: artifacts.GetArtifact("vegetable"), Quantity: 4}, Tall: false, Habitat: Land},
 	&PlantType{Name: "reed", MaturityAgeYears: 3, Yield: artifacts.Artifacts{A: artifacts.GetArtifact("reed"), Quantity: 1}, Tall: true, Habitat: Shore},
-	&PlantType{Name: "herb", MaturityAgeYears: 1, Yield: artifacts.Artifacts{A: artifacts.GetArtifact("herb"), Quantity: 1}, Tall: false, Habitat: Cultivated},
+	&PlantType{Name: "herb", MaturityAgeYears: 1, Yield: artifacts.Artifacts{A: artifacts.GetArtifact("herb"), Quantity: 1}, Tall: false, Habitat: Land},
 }
