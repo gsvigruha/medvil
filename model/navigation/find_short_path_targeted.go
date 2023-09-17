163c78d72ce3ae4f4a417a9f8da7b18842ee1b9f
Gergely Svigruha <sgergo88@gmail.com>
2023-09-16 20:27:58 -0400

    Nit

--- model/navigation/find_short_path_targeted.go
+++ model/navigation/find_short_path_targeted.go
@@ -45,10 +45,10 @@ func FindShortPathTargeted(m IMap, start Location, dest Destination, pathType Pa
 	var iter = 0
 	var currentEstimatedDist = 0
 	r := rand.New(rand.NewSource(int64(start.X*599 + start.Y)))
-	visited := make(map[Location]*[]PathElement, 20)
+	visited := make(map[Location]*[]PathElement, Capacity)
 	se := &TargetedElement{PE: m.GetField(start.X, start.Y).GetPathElement(start.Z), prev: nil, d: 1, ds: 0.0}
 	var toVisit = [][]*TargetedElement{[]*TargetedElement{se}}
-	var inQueue = make(map[Location]bool, 20)
+	var inQueue = make(map[Location]bool, Capacity)
 	for true {
 		var e *TargetedElement = nil
 		for e == nil {
