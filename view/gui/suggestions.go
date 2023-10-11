c9a98d6d344c5eff10d9394317fa06f3859e017f
Gergely Svigruha <sgergo88@gmail.com>
2023-10-11 18:25:07 -0400

    move suggestions down

--- view/gui/suggestions.go
+++ view/gui/suggestions.go
@@ -23,6 +23,10 @@ func (s *Suggestion) Render(cv *canvas.Canvas, iconS, iconD float64) {
 		}
 	}
 
+	var dh = 0.0
+	if s.Y < 200 {
+		dh = iconD
+	}
 	tw := maxLength * FontSize * 0.5
 	th := math.Max(float64(len(lines))*FontSize, iconD)
 
@@ -31,19 +35,19 @@ func (s *Suggestion) Render(cv *canvas.Canvas, iconS, iconD float64) {
 	cv.SetLineWidth(2)
 	cv.BeginPath()
 	cv.LineTo(s.X, s.Y)
-	cv.LineTo(s.X+120, s.Y-20)
-	cv.LineTo(s.X+120, s.Y-20-th/2.0)
-	cv.LineTo(s.X+120+tw+iconD, s.Y-20-th/2.0)
-	cv.LineTo(s.X+120+tw+iconD, s.Y+20+th/2.0)
-	cv.LineTo(s.X+120, s.Y+20+th/2.0)
-	cv.LineTo(s.X+120, s.Y+20)
+	cv.LineTo(s.X+120, s.Y-20+dh)
+	cv.LineTo(s.X+120, s.Y-20-th/2.0+dh)
+	cv.LineTo(s.X+120+tw+iconD, s.Y-20-th/2.0+dh)
+	cv.LineTo(s.X+120+tw+iconD, s.Y+20+th/2.0+dh)
+	cv.LineTo(s.X+120, s.Y+20+th/2.0+dh)
+	cv.LineTo(s.X+120, s.Y+20+dh)
 	cv.ClosePath()
 	cv.Fill()
 	cv.Stroke()
-	cv.DrawImage("icon/gui/"+s.Icon+".png", s.X+130, s.Y-iconS/2.0, iconS, iconS)
+	cv.DrawImage("icon/gui/"+s.Icon+".png", s.X+130, s.Y-iconS/2.0+dh, iconS, iconS)
 	cv.SetFillStyle("#FED")
 	cv.SetFont("texture/font/Go-Regular.ttf", FontSize)
 	for i, line := range lines {
-		cv.FillText(line, s.X+130+iconD, s.Y-float64(len(lines)-2)*FontSize/2.0-4+float64(i)*FontSize)
+		cv.FillText(line, s.X+130+iconD, s.Y-float64(len(lines)-2)*FontSize/2.0-4+float64(i)*FontSize+dh)
 	}
 }
