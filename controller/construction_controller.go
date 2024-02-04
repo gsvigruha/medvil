a22fbbf6f5dd4d0affb92f0b043135c6764399b9
Gergely Svigruha <sgergo88@gmail.com>
2024-02-04 00:48:57 -0500

    construction update

--- controller/construction_controller.go
+++ controller/construction_controller.go
@@ -20,6 +20,7 @@ var ArtifactOrder = []*artifacts.Artifact{
 
 func ConstructionToControlPanel(cp *ControlPanel, c *building.Construction) {
 	p := &gui.Panel{X: 0, Y: ControlPanelDynamicPanelTop, SX: ControlPanelSX, SY: HouseholdControllerSY}
+	p.AddTextLabel("Construction", 24, ControlPanelSY*0.15)
 	top := ConstructionControllerTop * ControlPanelSY
 	p.AddScaleLabel("tasks/building", 24, top, IconS, IconS, 4, float64(c.Progress)/float64(c.MaxProgress), false,
 		func(scaleStr string) {
@@ -33,13 +34,17 @@ func ConstructionToControlPanel(cp *ControlPanel, c *building.Construction) {
 			c.Delete()
 		}})
 	var i = 1
-	p.AddImageLabel("coin", 24, top+float64(IconH*2), IconS, IconS, gui.ImageLabelStyleRegular)
+	p.AddLabel(&gui.ImageLabel{Icon: "coin", X: 24, Y: top + float64(IconH*2), SX: IconS, SY: IconS, Style: gui.ImageLabelStyleRegular, OnHoover: func() {
+		cp.HelperMessage("Total materials needed for the building", false)
+	}})
 	for _, a := range c.Cost {
 		ArtifactsToControlPanel(cp, p, i, a.A, a.Quantity, top+float64(IconH*2))
 		i++
 	}
 	i = 1
-	p.AddImageLabel("barrel", 24, top+float64(IconH*4), IconS, IconS, gui.ImageLabelStyleRegular)
+	p.AddLabel(&gui.ImageLabel{Icon: "barrel", X: 24, Y: top + float64(IconH*4), SX: IconS, SY: IconS, Style: gui.ImageLabelStyleRegular, OnHoover: func() {
+		cp.HelperMessage("Total materials stored at this construction", false)
+	}})
 	for _, a := range ArtifactOrder {
 		if q, ok := c.Storage.Artifacts[a]; ok {
 			ArtifactsToControlPanel(cp, p, i, a, q, top+float64(IconH*4))
